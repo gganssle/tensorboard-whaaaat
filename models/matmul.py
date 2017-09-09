c5addbc0f226885e1212881fe0f059d55199d02c
gganssle <gr@m-gan.sl>
2017-09-09 14:42:47 -0500

    asdf

--- models/matmul.py
+++ models/matmul.py
@@ -8,11 +8,10 @@ d2 = np.random.rand(3,3)
 
 one = tf.placeholder('float', [3,3], name='one')
 two = tf.placeholder('float', [3,3], name='two')
-out = tf.placeholder('float', [3,3], name='out')
 
-mul_op = tf.matmul(one, two, name='multiply')
-
-tf.summary.histogram(one, 'thing')
+with tf.name_scope('multipliy'):
+	mul_op = tf.matmul(one, two, name='mult')
+	tf.summary.scalar('thing', mul_op)
 
 with tf.Session() as sess:
 	writer = tf.summary.FileWriter('./logs/matmul', sess.graph)
@@ -21,3 +20,5 @@ with tf.Session() as sess:
 	tf.global_variables_initializer().run()
 
 	sess.run(mul_op, feed_dict={one: d1, two: d2})
+	#summary = sess.run([merged, mul_op], feed_dict={one: d1, two: d2})
+	writer.add_summary(summary)
